c47e7b27428eada2f387cc37e9f4ee43857009cc
汪秋月 <335098623@qq.com>
2018-12-23 00:53:07 +0800

    增加基础样式

---
+++ pages/index/index.js
@@ -0,0 +1,209 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navList: [{
+      title: '锚点1',
+      background:'#f17c67',
+      content: [{
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }, {
+        article: "列表一"
+      }]
+    }, {
+      title: '锚点2',
+        background: '#96ceb4',
+      content: [{
+        article: "列表二"
+      }, {
+        article: "列表二"
+      }, {
+        article: "列表二"
+      }, {
+        article: "列表二"
+      }, {
+        article: "列表二"
+      }, {
+        article: "列表二"
+      }, {
+        article: "列表二"
+      }]
+    }, {
+      title: '锚点3',
+        background: '#d9d9f3',
+      content: [{
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }, {
+        article: "列表三"
+      }]
+    }, {
+      title: '锚点4',
+        background: '#fad3cf',
+      content: [{
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }, {
+        article: "列表四"
+      }]
+    }, {
+      title: '锚点5',
+        background: '#f6f6e9',
+      content: [{
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }, {
+        article: "列表五"
+      }]
+    }, {
+      title: '锚点6',
+        background: '#6abe83',
+      content: [{
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+      }, {
+        article: "列表六"
+        }, {
+          article: "列表六"
+        }, {
+          article: "列表六"
+        }, {
+          article: "列表六"
+        }, {
+          article: "列表六"
+        }, {
+        article: "列表六"
+      }]
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
